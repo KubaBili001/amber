c39cd2487d9a8f6715f4f6e1480ff78374e9ffe0
KubaBili001 <kubabili.wawa@gmail.com>
2024-10-29 23:13:09 +0100

    images

--- amber/src/components/Content.tsx
+++ amber/src/components/Content.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home/Home";
-import Body from "./Products/Body";
 
 const Content: React.FC = () => {
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="body" element={<Body />} />
+        <Route path="body" element={<></>} />
         {/* add additional routes when needed */}
       </Routes>
     </>
---
+++ amber/src/components/Products/ProductsList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsList: React.FC = () => {
+  return <></>;
+};
+
+export default ProductsList;
