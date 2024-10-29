53663b7ad514b91cfb57cb79b4bbe151b588fe14
KubaBili001 <kubabili.wawa@gmail.com>
2024-10-30 00:03:03 +0100

    product data part 1

---
+++ amber/src/data/productsData.ts
@@ -0,0 +1,253 @@
+// Auto-generated product data
+export const bodyData = [
+  {
+    "primaryImage": "/src/assets/images/body/1.jpg",
+    "secondaryImage": "/src/assets/images/body/1_2.jpg",
+    "name": "Hand Cream Nourishing Bottle Amberika 100 ml",
+    "content": "24 hours of moisturizing and protection. Succinic acid promotes active renewal and rejuvenation of the skin, is an excellent antioxidant. Amber oil and shea butter nourish the skin, protect it from overdrying and aggressive environmental influences. Lactic acid has a light exfoliating effect and moisturizes the skin. Olive oil softens dry skin, saturates it with vitamins. Directions for use: Apply the cream to clean skin of your hands. Ingredients: Water with Succinic Acid, Shea Butter, Lecithin, Sorbitol, Olive Oil, Allantoin, Benzyl Alcohol",
+    "price": 450,
+    "baseName": "1"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/10.jpg",
+    "secondaryImage": "/src/assets/images/body/10_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "10"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/11.jpg",
+    "secondaryImage": "/src/assets/images/body/11_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "11"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/12.jpg",
+    "secondaryImage": "/src/assets/images/body/12_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "12"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/13.jpg",
+    "secondaryImage": "/src/assets/images/body/13_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "13"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/14.jpg",
+    "secondaryImage": "/src/assets/images/body/14_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "14"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/15.jpg",
+    "secondaryImage": "/src/assets/images/body/15_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "15"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/16.jpg",
+    "secondaryImage": "/src/assets/images/body/16_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "16"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/17.jpg",
+    "secondaryImage": "/src/assets/images/body/17_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "17"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/18.jpg",
+    "secondaryImage": "/src/assets/images/body/18_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "18"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/19.jpg",
+    "secondaryImage": "/src/assets/images/body/19_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "19"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/2.jpg",
+    "name": "Moisturizing hand cream Amberika 75 ml",
+    "content": "Well-groomed and moisturized skin of the hands. Amber hand cream is specially created for instant moisturizing and nourishing of the skin. Amber powder in the composition, with a particle size of up to 10 microns, actively helps to restore tone, elasticity and firmness, affects the cause of skin aging, normalizing energy processes at the intracellular and tissue levels. Vitamin E stimulates cellular respiration, strengthens the membranes of skin cells. Vitamin F softens the skin, relieves irritation of rough, dry, cracked skin, promotes rapid restoration of skin cells and maintains their smoothness. Calendula officinalis flower extract has powerful antibacterial, anti-inflammatory, soothing and rejuvenating properties. Avocado oil provides protection from negative environmental influences, prevents irritation and peeling of the skin, as well as its aging. Directions for use: Apply to clean, dry skin of hands 1-2 times a day. Ingredients: Aqua, helianthus annuus (sunflower) seed oil, stearic acid, ceto stearyl alcohol, glycerin, perseagratissima oil, acrylate/c10-c30 acrylicresin the copolymer, cyclopentasiloxane (and) cyclohexasiloxan",
+    "price": 250,
+    "baseName": "2"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/20.jpg",
+    "secondaryImage": "/src/assets/images/body/20_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "20"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/21.jpg",
+    "secondaryImage": "/src/assets/images/body/21_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "21"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/22.jpg",
+    "secondaryImage": "/src/assets/images/body/22_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "22"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/23.jpg",
+    "secondaryImage": "/src/assets/images/body/23_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "23"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/24.jpg",
+    "secondaryImage": "/src/assets/images/body/24_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "24"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/25.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "25"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/26.jpg",
+    "secondaryImage": "/src/assets/images/body/26_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "26"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/27.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "27"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/28.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "28"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/29.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "29"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/3.jpg",
+    "name": "Body Oil Firming with dispenser Amberika 100 ml",
+    "content": "Firm, elastic and toned skin. Amber oil easily penetrates into the deep layers of the skin, activates blood circulation and cellular metabolism. It has an excellent property to restore damaged tissue, thus being an effective means in the fight against cellulite. A carefully selected complex of natural oils of wheat germ, jojoba, almond and hazelnut makes your skin soft and tender. The combination of essential oils of grapefruit, clove, ginger and orange increases firmness and improves elasticity of the skin. Directions for use: Warm a small amount of body oil in your palms. Apply to damp, clean skin with gentle massage movements, paying particular attention to problem areas. Particularly effective when performing anti-aging procedures. Ingredients: Rapeseed Oil, Sea Buckthorn Oil, Citrus sinensis Essential Oil, Wheat Germ Oil, Polysorbate-80,",
+    "price": 850,
+    "baseName": "3"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/30.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "30"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/31.jpg",
+    "secondaryImage": "/src/assets/images/body/31_2.jpg",
+    "name": "Shower gel with scrub effect Amberika 100 ml",
+    "content": "Sulfate-free shower gel with succinic acid and natural extracts gently cleanses and moisturizes the skin, stimulates metabolic processes. Scrubbing particles gently exfoliate dead epidermal cells, and the action of the active components preserves the youth and elasticity of the skin. Succinic acid is an excellent antioxidant, slows down the oxidation processes in cells, and therefore prolongs the youth of the whole body. Lactic acid has a light exfoliating effect, moisturizes and renews the skin. Panthenol deeply nourishes and restores the skin, makes it elastic and smooth. Result: delicately cleansed, smooth and toned skin. Directions for use: Apply a small amount of gel to damp skin with massaging movements and rinse with warm water. Ingredients: Water, cocamidopropyl betaine, alpha olefine sulfonate, coco glucoside, sorbitol, benzyl alcohol, phe",
+    "price": 350,
+    "baseName": "31"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/4.jpg",
+    "name": "Body oil Moisturizing with dispenser Amberica 100 ml",
+    "content": "Perfectly moisturized, amazingly soft and tender skin. Moisturizing is the key to beautiful and healthy skin. This is an important stage of skin care for any skin type at any age to maintain its youth and elasticity. Amber oil has excellent regenerating properties, fills the skin with energy, promoting natural moisturizing. The ideal combination of specially selected natural vegetable oils of jojoba, macadamia and argan deeply moisturize your skin, maintaining its natural pH. Avocado oil in combination with vitamin E enhances the hydrolipid balance of the skin and protects it from moisture loss. Walnut oil actively nourishes and saturates it with essential vitamins. Wheat germ oil and verbena essential oil start the processes of skin rejuvenation and regeneration. Directions for use: Warm a small amount of body oil in your palms. Apply to damp, clean skin with gentle massage movements, paying particular attention to problem areas. Ingredients: Grape seed oil, walnut oil, macadamia oil, caprylic/capric triglycerides, avocado oil, wheat germ oil, soybean oil, jojoba oil, sea buckthorn oil, argan oil, cone oil",
+    "price": 850,
+    "baseName": "4"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/5.jpg",
+    "secondaryImage": "/src/assets/images/body/5_2.jpg",
+    "name": 'Body Scrub "Marzipan" Amberika 200 ml',
+    "content": "Almond oil instantly moisturizes and nourishes the skin. Cane sugar gently exfoliates dead skin cells, leaving your skin smooth and even. Directions for use: Apply the required amount of scrub to damp body using massage movements, then rinse with water. Ingredients: Cane Sugar, Sugar, Rapeseed Oil, Polyglyceryl Stearate, Succinic Acid, Lecithin, Almond Oil",
+    "price": 690,
+    "baseName": "5"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/6.jpg",
+    "secondaryImage": "/src/assets/images/body/6_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "6"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/7.jpg",
+    "secondaryImage": "/src/assets/images/body/7_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "7"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/8.jpg",
+    "secondaryImage": "/src/assets/images/body/8_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "8"
+  },
+  {
+    "primaryImage": "/src/assets/images/body/9.jpg",
+    "secondaryImage": "/src/assets/images/body/9_2.jpg",
+    "name": "",
+    "content": "",
+    "price": 0,
+    "baseName": "9"
+  }
+];
+
+export const faceData = [];
+
+export const hairData = [];
+
+export const spaData = [];
+
+export const health_improvementData = [];
+
+export default { bodyData, faceData, hairData, spaData, health_improvementData };
